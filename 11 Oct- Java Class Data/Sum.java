9975fab0034719c7231f3cdca5a9de5cd3729162
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2017-10-11 20:36:02 +0530

    Add files via upload

---
+++ 11 Oct- Java Class Data/Sum.java
@@ -0,0 +1,15 @@
+//program sum--> of 2 numbers
+
+class Sum{
+	//access specifier--> 
+	static public void main(String[] a)
+	{
+		int firstValue = 10;
+		int secondValue = 20;
+		int result = firstValue + secondValue;		
+
+System.out.println("Sum=" + (firstValue + secondValue));		
+	}	
+	
+
+}
